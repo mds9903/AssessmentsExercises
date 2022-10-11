906c28bd532033440fec70733c1dbfab86d78c09
mds9903 <mohammed.sujan@nextuple.com>
2022-10-11 10:20:20 +0530

    working completely; using LocalDateTime and Temporal.ChronoUnit

--- src/TimeModule.java
+++ src/TimeModule.java
@@ -5,22 +5,21 @@ import java.time.format.DateTimeParseException;
 import java.util.*;
 
 import static java.time.temporal.ChronoUnit.DAYS;
+import static java.time.temporal.ChronoUnit.HOURS;
 
 public class TimeModule {
     static String dmyPattern = "dd/MM/uuuu";
 
     public static void main(String[] args) {
-
         // Find the days remaining for next birthday
         // Ask for the birth date and
         // if date falls in last month say already done
         // else show months / days / hours remaining
-
         // code
 //        Scanner sc = new Scanner(System.in);
 //        System.out.println("Enter your birth date in 'DD/MM/YYYY' format");
 //        String input = sc.next();// take user input of date as string
-//        LocalDate dob = getDateObj(input);
+//        LocalDateTime dob = getDateObj(input); // convert to date
 //        while (dob == null) {// validate input for date
 //            // else ask the user to input again
 //            System.out.println("invalid date entered.Please enter in 'DD/MM/YYYY' format" +
@@ -28,34 +27,33 @@ public class TimeModule {
 //            input = sc.next();
 //            dob = getDateObj(input);
 //        }
-//        String input = "23/11/2001";
-//        String input = "12/09/1997";
+
         String input = "15/10/1997";
         LocalDate dob = getDateObj(input);
+
         // if valid
-        // find the number of days between today and birth date
-        LocalDate today = LocalDate.now();
-//        System.out.println(DAYS.between(today, dob)); // give error why?
-        LocalDate thisYearBday = getDateObj(input.substring(0, 6) + today.getYear());
-        System.out.println("You bday this year " + thisYearBday);
-        assert thisYearBday != null;
-        long diffDays = DAYS.between(thisYearBday, today);
-        if(diffDays >= 30) // bday was last month
+        // find the number of hours between currentDT and birth date
+        LocalDateTime currentDT = LocalDateTime.now();
+        System.out.println("current dt "+ getDateString(currentDT) +" "+ currentDT.toString().substring(11));
+
+        LocalDateTime bdayDT = getDateObj(input.substring(0, 6) + currentDT.getYear()).atTime(0,0);
+        System.out.println("You bday this year " + getDateString(bdayDT));
+        if(bdayDT.getMonthValue() < currentDT.getMonthValue()) // bday was last month
         {
             System.out.println("Your birthday already done");
         }else{
-            int hours = 24 * (int)Math.abs(diffDays);
-//            System.out.println("Days remaining until your bday " + Math.abs(diffDays) + " days");
+            long diffHours = Math.abs(HOURS.between(bdayDT, currentDT));
+//            System.out.println(diffHours);
             // print the difference as
             // m months, d days and h hours remaining
-            System.out.println(hours / (30*24)+" months"); // months
-            hours %= 30 * 24;
-            System.out.println(hours / 24+" days");
-            hours %= 24;
-            System.out.println(hours+" hours");
+            int months = (int) (diffHours / (30*24));
+            diffHours %= (30*24);
+            int days = (int) diffHours / 24;
+            diffHours %= 24;
+            System.out.println((months > 0 ? months+" months, " : "")
+                                +""+ (days > 0 ? days+" days, and " : "")
+                                +""+(diffHours > 0 ? diffHours+" hours until your birthday" : ""));
         }
-
-
     }
 
     private static LocalDate getDateObj(String input) {
@@ -68,7 +66,7 @@ public class TimeModule {
         }
     }
 
-    private static String getDateString(LocalDate date) {
+    private static String getDateString(LocalDateTime date) {
         return date.format(DateTimeFormatter.ofPattern(TimeModule.dmyPattern)).toString().substring(0, 10);
     }
 
