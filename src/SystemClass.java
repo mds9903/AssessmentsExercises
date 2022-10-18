5510a887b99f98150975b22dbd544e7615ea3b30
mds9903 <mohammed.sujan@nextuple.com>
2022-10-18 10:24:25 +0530

    able to get the os name and the java version; not sure how to find the distro name; looking for solutions

---
+++ src/SystemClass.java
@@ -0,0 +1,10 @@
+//"Say for your application specific OS and version of Java is needed.
+// Say its Ubuntu and Java 8 and above.
+// How will you apply this check?"
+public class SystemClass {
+    public static void main(String[] args) {
+        String osName = System.getProperty("os.name");
+        String javaVersion = System.getProperty("java.version");
+        System.out.println("OS name: "+osName +", Java Version: "+javaVersion);
+    }
+}
